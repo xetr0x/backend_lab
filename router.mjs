c68e0a2f3bd6a8c1861f67bc252cc01c0f7efecd
Xetr0x <kwilusz10@gmail.com>
2026-03-12 15:49:45 +0100

    ground for server and router made

---
+++ router.mjs
@@ -0,0 +1,5 @@
+import express from "express";
+
+const main_router = express.Router();
+
+export default main_router;
--- server.mjs
+++ server.mjs
@@ -1,2 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import bcrypt from "bcryptjs";
+import main_router from "./router.mjs";
+
+dotenv.config();
+
+const app = express();
+const PORT = 8080;
+// Do authentication middleware
+
+app.use("/", main_router);
+
+app.listen(PORT, "127.0.0.1", () => {
+  console.log(`Server running at localhost:${PORT}`);
+});
